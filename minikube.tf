b1f7eb98a3f18d21d9644349a74e550e48d93789
chaitu047 <chaitu.klk047@gmail.com>
2024-02-24 23:13:07 +0530

    second commit

--- minikube.tf
+++ minikube.tf
@@ -1,14 +1,14 @@
 module "minikube" {
   source = "github.com/scholzj/terraform-aws-minikube"
 
-  aws_region    = "us-east-1"
-  cluster_name  = "roboshop"
+  aws_region        = "us-east-1"
+  cluster_name      = "roboshop"
   aws_instance_type = "t3.medium"
-  key_name   = "dev-ssh"
-  aws_subnet_id = "subnet-07f16c157b7defce9" #replace your default subnet id
+  ssh_public_key    = "~/.ssh/aws-devops-git.pub"
+  aws_subnet_id     = "subnet-07f16c157b7defce9" #replace your default subnet id
   # by default centos7 will be used
   #ami_image_id = "ami-b81dbfc5"
-  hosted_zone = "switchtodevops.xyz"
+  hosted_zone         = "switchtodevops.xyz"
   hosted_zone_private = false
 
   tags = {
--- workstation.tf
+++ workstation.tf
@@ -1,14 +1,14 @@
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
+  source = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "workstation"
-  ami = data.aws_ami.centos8.id
-  instance_type          = "t2.micro"
-  key_name   = "dev-ssh"
+  name          = "workstation"
+  ami           = data.aws_ami.centos8.id
+  instance_type = "t2.micro"
+  key_name      = "dev-ssh"
   #monitoring             = true
   vpc_security_group_ids = [aws_security_group.allow_minikube.id]
   subnet_id              = "subnet-07f16c157b7defce9" #replace your default subnet id
-  user_data = file("docker.sh")
+  user_data              = file("docker.sh")
   tags = {
     Terraform   = "true"
     Environment = "dev"
@@ -31,29 +31,29 @@ resource "aws_security_group" "allow_minikube" {
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-data "aws_ami" "centos8"{
-    owners = ["973714476881"]
-    most_recent      = true
+data "aws_ami" "centos8" {
+  owners      = ["973714476881"]
+  most_recent = true
 
-    filter {
-        name   = "name"
-        values = ["Centos-8-DevOps-Practice"]
-    }
+  filter {
+    name   = "name"
+    values = ["Centos-8-DevOps-Practice"]
+  }
 
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
