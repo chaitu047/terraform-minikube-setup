06edd451dfc6e33a5da9dfcfa4edda7868ed6bb4
chaitu047 <chaitu.klk047@gmail.com>
2024-02-24 22:57:06 +0530

    initial commit

---
+++ minikube.tf
@@ -0,0 +1,24 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  key_name   = "dev-ssh"
+  aws_subnet_id = "subnet-07f16c157b7defce9" #replace your default subnet id
+  # by default centos7 will be used
+  #ami_image_id = "ami-b81dbfc5"
+  hosted_zone = "switchtodevops.xyz"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
